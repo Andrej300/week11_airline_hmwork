9c57fca315f33c38750be6068e999b9589ed16d6
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2021-02-14 21:17:02 +0000

    first two test run

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+
+        Passenger passenger;
+
+        @Before
+
+        public void before() {
+            passenger = new Passenger("Arnold", 1);
+        }
+
+        @Test
+        public void passengerHasName() {
+
+            assertEquals("Arnold", passenger.getName());
+        }
+
+        @Test
+        public void passengerHasBag() {
+            assertEquals(1, passenger.getNumberOfBags());
+        }
+
+    }
+    
+
