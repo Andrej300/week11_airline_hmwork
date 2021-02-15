587b223b62800b6c9ea456a26aa0413035ed26d7
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2021-02-15 01:17:45 +0000

    planehastype passed

---
+++ src/main/java/PlaneType.java
@@ -0,0 +1,17 @@
+public enum PlaneType {
+
+    BOEING747(200, 900),
+    AIRBUSA380(300, 950),
+    BOEING717(150,850);
+
+    private final int capacity;
+    private final int totalWeight;
+
+    PlaneType(int capacity, int totalWeight) {
+        this.capacity = capacity;
+        this.totalWeight = totalWeight;
+    }
+
+
+
+}
